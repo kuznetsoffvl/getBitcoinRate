ca63fc5c633d067ec4f48964ef12c5aa6462f069
Vladimir Kuznetsov <kuznetsoff.vl@gmail.com>
2018-04-02 19:53:16 +0300

    adds ; in the end of file

---
+++ js/index.js
@@ -0,0 +1,20 @@
+﻿var prvRate = 0;
+
+function getBitcoinRate() {
+  $.get(
+    url = "https://api.coindesk.com/v1/bpi/currentprice.json", (response)=> {    
+    let obj = JSON.parse(response);
+    let now = new Date();
+    let dif = '' + (Number(obj.bpi.USD.rate.replace(',', '')) - prvRate).toFixed(4);
+    let trend = dif > 0 ? '↑+' : dif < 0 ? '↓' : '~';
+    //let trend = dif > 0 ? '↑+' :  '↓' ;
+
+    $("div").html(obj.bpi.USD.rate + ' (' +trend + dif + '): ' + obj.chartName + " rate on " + now.toString());    
+    prvRate = Number(obj.bpi.USD.rate.replace(",", "")).toFixed(4);
+      //console.log(prvRate);
+  });
+}
+
+getBitcoinRate();
+$("button").on("click", ()=> getBitcoinRate());
+setInterval(getBitcoinRate, 1000);;
